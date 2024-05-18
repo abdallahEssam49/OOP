2ccd2b12dd6e868c1bbe62cc5473ae83a5f40103
abdallahEssam49 <76582213+abdallahEssam49@users.noreply.github.com>
2024-05-18 15:20:21 +0300

    Update employee.cpp

--- CPP/Structs/employee.cpp
+++ CPP/Structs/employee.cpp
@@ -14,7 +14,7 @@ employee employees_arr[MAX];
 int added = 0;	// Number of employees
 
 void read_employee() {
-  cout<<"Enter employee 4 entries: ";
+  cout<<"Enter employee 4 entries: name , age , salary , gender"<<endl;
   cin >> employees_arr[added].name >> employees_arr[added].age;
   cin >> employees_arr[added].salary >> employees_arr[added].gender;
   added++;
@@ -28,12 +28,12 @@ void print_employees() {
 }
 
 int main() {
-  employee first = { "mostafa", 10, 1200.5, "Male" };
+  employee first = { "mostafa", 22, 12000.5, "Male" };
   employees_arr[added++] = first;
 
   employees_arr[added].name = "hani";
   employees_arr[added].age = 55;
-  employees_arr[added].salary = 750;
+  employees_arr[added].salary = 75000;
   employees_arr[added].gender = "Male";
   added++;
 
